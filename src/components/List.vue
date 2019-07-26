c01bd601467419c64bc089f922363d7d9a498363
Marco Reitano <marco.reitano@th-koeln.de>
2019-07-26 16:38:39 +0200

    Corrects api address to api.marcoreitano.dev

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="concert-list">
-  <h1 class="list-title">Concert-List</h1>
-  <list-item v-for="concert of concerts" v-bind:key="concert.date" v-bind:concert="concert"></list-item>
+    <h1 class="list-title">Concert-List</h1>
+    <list-item v-for="concert of concerts" v-bind:key="concert.date"
+               v-bind:concert="concert"></list-item>
   </div>
 </template>
 
@@ -12,14 +13,14 @@
   export default {
     name: "List",
     components: {ListItem},
-    data: function(){
-      return{
+    data: function () {
+      return {
         resultData: '',
         concerts: ''
       }
     },
     mounted() {
-      axios({ method: "GET", "url": "http://mitneve.io/concerts"}).then(result => {
+      axios({method: "GET", "url": "https://api.marcoreitano.dev/concerts"}).then(result => {
         this.concerts = result.data._embedded.concerts;
       }, error => {
         console.error(error)
@@ -29,10 +30,11 @@
 </script>
 
 <style scoped>
-  .list-title{
+  .list-title {
     color: black;
   }
-  .concert-list{
+
+  .concert-list {
     display: block;
     color: white;
     font-family: Arial, Helvetica, sans-serif;
